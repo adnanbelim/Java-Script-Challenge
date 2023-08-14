9dc4b3e5e1b075f0b52e1ca5178e8edd0b4f4ab0
adnanbelim <belimadnan111@gmail.com>
2023-08-14 21:29:10 +0530

    Java Script

---
+++ function.js
@@ -0,0 +1,57 @@
+// function multiply(){
+
+//     for(var i=1;i<=10;i++){
+//         document.write(2*i);
+//         document.write("<br>");
+    
+//     }
+// }
+
+
+// multiply();
+
+// function multable(num){
+//     for(i=1;i<=10;i++){
+//         // document.write(`${num} x ${i} = ${num*i}`);
+//         document.write(num + 'x' + i + '=' + num*i);
+//         document.write("<br>");
+//     }
+
+// }
+
+// multable(5);
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+let c = add(5,6);
+
+document.write(c);
+
+
+
+/* *******infinite parameter******** */
+
+// function add(){
+
+//     if(arguments.length == 0){
+//         document.write("no arguments passed !" );
+//     }
+
+//     else{
+//         let sum = 0;
+
+//         for(let i = 0; i < arguments.length; i++)
+//         {
+
+//         sum = sum + arguments[i];
+        
+
+//         }
+//         document.write(sum);
+//     }
+// }
+
+// add(5,5);
+
